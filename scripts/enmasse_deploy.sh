0c8041dc6c295560f00d9fb902f7323576953a81
ppatierno <ppatierno@live.com>
2017-05-12 12:50:07 +0200

    Added routes for adapters and Hono server Updated deployment files to the current Hono repo status

--- scripts/enmasse_deploy.sh
+++ scripts/enmasse_deploy.sh
@@ -10,7 +10,7 @@ oc create sa enmasse-service-account -n $(oc project -q)
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default
 oc policy add-role-to-user edit system:serviceaccount:$(oc project -q):enmasse-service-account
 
-oc process -f https://github.com/EnMasseProject/enmasse/releases/download/0.8.0/sasldb-enmasse-template.yaml | oc create -f -
+oc process -f https://github.com/EnMasseProject/enmasse/releases/download/0.9.0/sasldb-enmasse-template.yaml | oc create -f -
 echo ... done
 
 echo ECLIPSE HONO AND ENMASSE DEPLOYED ON OPENSHIFT
--- scripts/hono_deploy.sh
+++ scripts/hono_deploy.sh
@@ -12,14 +12,18 @@ oc create -f ../application/target/fabric8/hono-app-pvc.yml
 echo Deploying Hono Server ...
 oc create -f ../application/target/fabric8/hono-app-svc.yml
 oc create -f ../application/target/fabric8/hono-app-deployment.yml
+oc create -f ../application/target/fabric8/hono-app-route.yml
 echo ... done
 
 echo Deploying HTTP REST adapter ...
 oc create -f ../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-svc.yml
 oc create -f ../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-deployment.yml
+oc create -f ../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-route.yml
+oc create -f ../adapters/rest-vertx/target/fabric8/hono-adapter-rest-vertx-insecure-route.yml
 echo ... done
 
 echo Deploying MQTT adapter ...
 oc create -f ../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-svc.yml
 oc create -f ../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-deployment.yml
+oc create -f ../adapters/mqtt-vertx/target/fabric8/hono-adapter-mqtt-vertx-route.yml
 echo ... done
